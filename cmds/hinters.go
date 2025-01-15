ffba8738ee68209058a37a8ce8d7fb338680e6c9
soonkuk <soonkuk@gmail.com>
2025-01-15 15:48:53 +0900

    feat: currency model updated

--- cmds/hinters.go
+++ cmds/hinters.go
@@ -5,7 +5,6 @@ import (
 	"github.com/ProtoconNet/mitum-did-registry/operation/did"
 	"github.com/ProtoconNet/mitum-did-registry/state"
 	"github.com/ProtoconNet/mitum-did-registry/types"
-	"github.com/ProtoconNet/mitum2/launch"
 	"github.com/ProtoconNet/mitum2/util/encoder"
 	"github.com/pkg/errors"
 )
@@ -40,25 +39,11 @@ var AddedSupportedHinters = []encoder.DecodeDetail{
 }
 
 func init() {
-	defaultLen := len(launch.Hinters)
-	currencyExtendedLen := defaultLen + len(currencycmds.AddedHinters)
-	allExtendedLen := currencyExtendedLen + len(AddedHinters)
+	Hinters = append(Hinters, currencycmds.Hinters...)
+	Hinters = append(Hinters, AddedHinters...)
 
-	Hinters = make([]encoder.DecodeDetail, allExtendedLen)
-	copy(Hinters, launch.Hinters)
-	copy(Hinters[defaultLen:currencyExtendedLen], currencycmds.AddedHinters)
-	copy(Hinters[currencyExtendedLen:], AddedHinters)
-
-	defaultSupportedLen := len(launch.SupportedProposalOperationFactHinters)
-	currencySupportedExtendedLen := defaultSupportedLen + len(currencycmds.AddedSupportedHinters)
-	allSupportedExtendedLen := currencySupportedExtendedLen + len(AddedSupportedHinters)
-
-	SupportedProposalOperationFactHinters = make(
-		[]encoder.DecodeDetail,
-		allSupportedExtendedLen)
-	copy(SupportedProposalOperationFactHinters, launch.SupportedProposalOperationFactHinters)
-	copy(SupportedProposalOperationFactHinters[defaultSupportedLen:currencySupportedExtendedLen], currencycmds.AddedSupportedHinters)
-	copy(SupportedProposalOperationFactHinters[currencySupportedExtendedLen:], AddedSupportedHinters)
+	SupportedProposalOperationFactHinters = append(SupportedProposalOperationFactHinters, currencycmds.SupportedProposalOperationFactHinters...)
+	SupportedProposalOperationFactHinters = append(SupportedProposalOperationFactHinters, AddedSupportedHinters...)
 }
 
 func LoadHinters(encs *encoder.Encoders) error {
